417aef938f451b37aee4657ecbef7fe24276ac53
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-03-29 14:40:48 +0530

    Setup dependancies

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log("Port running on:", PORT);
+});
